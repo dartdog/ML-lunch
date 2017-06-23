a2a7860e81a877ae920acec02a2131f66a494473
Tom Brander <tom@oswco.com>
2017-06-23 13:01:32 -0500

    Added notebook slide enhancements
    Better style and transitions and scroll bars

--- tpot_iris_pipeline.py
+++ tpot_iris_pipeline.py
@@ -1,9 +1,10 @@
 import numpy as np
 
-from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.svm import LinearSVC
+from tpot.builtins import StackingEstimator
 
 # NOTE: Make sure that the class is labeled 'class' in the data file
 tpot_data = np.recfromcsv('PATH/TO/DATA/FILE', delimiter='COLUMN_SEPARATOR', dtype=np.float64)
@@ -12,8 +13,8 @@ training_features, testing_features, training_target, testing_target = \
     train_test_split(features, tpot_data['class'], random_state=42)
 
 exported_pipeline = make_pipeline(
-    PCA(iterated_power=2),
-    DecisionTreeClassifier(criterion="entropy", max_depth=3, min_samples_split=18)
+    StackingEstimator(estimator=BernoulliNB(alpha=0.001, fit_prior=True)),
+    LinearSVC(dual=True, loss="squared_hinge", penalty="l2", tol=0.1)
 )
 
 exported_pipeline.fit(training_features, training_target)
